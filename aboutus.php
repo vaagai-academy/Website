358ca75c06efb3937152d7db0f6795aef2dd44fd
vaagai-academy <88882743+vaagai-academy@users.noreply.github.com>
2022-02-17 18:18:59 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,36 @@
+<?php 
+	include "header.php";
+?>
+
+<!-- About us -->
+<section id="about">
+	<div class="py-5">
+		<h2 class="text-center">About Us</h2>
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-md-6 col-12">
+				<h1 class="lead text-center"><strong>VAAGAI ACADEMY</strong></h1>
+				<p class="py-3 text-justify">Vaagai Academy was founded by highly motivated academicians, technocrats and entrepreneurs, who hold expertise knowledge across various domains. It imparts good-quality technical training in a variety of professional courses. We focus on practical learning with industry experts and experienced trainers. We offer quality training in assorted courses like C, C++, JAVA, PYTHON, JAVASCRIPT, WEB DEVELOPMENT and SOFT SKILLS Courses.
+				</p>
+				<p class="text-justify">Vaagai Academy delivery structure is designed to be a unified scalable and sustainable model; the service offerings play to its strength leveraging the output of each service function. Realizing the importance, we are able to render varied educational projects for the individuals. Experts from the Industry will deliver extra sessions on “How the technology works in Industrial Scenario”.</p>
+			</div>
+			<div class="col-lg-6 col-md-6 col-12">
+				<h3>Vision</h3>
+				<p>To be the favored Institution for IT learning, Preparatory courses training, Soft Skills Development and other career oriented Skill Development training programs. </p>
+				<br>
+				<h3>Mission</h3> 
+				<ul>
+					<li>To bridge the gap in employable manpower through professional training.</li><br>
+					<li>To assist students, professionals, corporate, institutions and government entities in keeping their IT and other skill set and capabilities up-to-date.</li><br>
+					<li>To deliver high quality courses using a unique learning methodology of instructor.</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php 
+	include "footer.php";
+?>
---
+++ careers.php
@@ -0,0 +1,19 @@
+<?php 
+	include "header.php";
+?>
+<br><br>
+<section>
+	<div class="container">
+		<h2>Careers</h2>
+		<p>
+			Join Us! Be a part of a dynamic, result in a bound organization that’s growing and creating its presence felt altogether corners of the world. From being a part of a world category team to enjoying a variety of structure perks, you have got a variety of reasons for the change of integrity of our personnel.
+		</p>
+		<p>
+			As a part of the team, your contributions are valued and our work setting can assist you to reach for the sky by serving to you improve your expertise in multiple domains, work aboard the industry’s high thought leaders, and explore opportunities for growth and development. You’ll get a piece setting tributary to the delivery of superior performances to nurture your personal and skilled goals and bring home the bacon your targets and people of the organization through cooperative efforts and a productive atmosphere.
+		</p>
+	</div>
+</section>
+
+<?php 
+	include "footer.php";
+?>
